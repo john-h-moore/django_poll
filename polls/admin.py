16c763004d0bf55ba9c4b0eabd85395d3da584e3
John H Moore <john@jhmwebdesign.com>
2013-11-15 13:33:50 -0500

    Made "Date Information" field collapsible

--- polls/admin.py
+++ polls/admin.py
@@ -5,7 +5,7 @@ class PollAdmin(admin.ModelAdmin):
 	#fields = ['pub_date', 'question']
 	fieldsets = [
 		(None,	{'fields': ['question']}),
-		('Date information', 	{'fields': ['pub_date']}),
+		('Date information', 	{'fields': ['pub_date'], 'classes': ['collapse']}),
 	]
 
 admin.site.register(Poll, PollAdmin)
