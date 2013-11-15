29194d84fd81c10456bacaa578c8d8bce9517315
John H Moore <john@jhmwebdesign.com>
2013-11-15 14:10:25 -0500

    Corrected typo

--- polls/views.py
+++ polls/views.py
@@ -5,7 +5,7 @@ from django.template import RequestContext, loader
 from polls.models import Poll
 
 def index(request):
-	latest_poll_list = Polls.objects.order_by('-pub_date')[:5]
+	latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
 	template = loader.get_template('polls/index.html')
 	context = RequestContext(request, {
 			'latest_poll_list': latest_poll_list,
