afda7545257f01c8d55dc2e4369dc4e05e606a20
John H Moore <john@jhmwebdesign.com>
2013-11-15 14:15:04 -0500

    Improved poll detail view

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
 
 from polls.models import Poll
@@ -10,7 +10,12 @@ def index(request):
 	return render(request, 'polls/index.html', context)
 
 def detail(request, poll_id):
-	return HttpResponse("You're looking at poll %s." %poll_id)
+	try:
+		poll = Poll.objects.get(pk=poll_id)
+	except Poll.DoesNotExist:
+		raise Http404
+	return render(request, 'polls/detail.html', {'poll': poll})
+	#return HttpResponse("You're looking at poll %s." %poll_id)
 
 def results(request, poll_id):
 	return HttpResponse("You're looking at the results of poll %s." %poll_id)
