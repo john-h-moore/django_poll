f9ae2deac2cc7977b653b812f33ebc0505d8b57d
John H Moore <john@jhmwebdesign.com>
2013-11-15 20:04:10 -0500

    Added "generic" import from django.views

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
+from django.views import generic
 from polls.models import Poll
 
 '''
