a756bf7860bf854fdbfae6c069a04b62d7b1f95e
John H Moore <john@jhmwebdesign.com>
2013-11-15 21:04:09 -0500

    Hopefully fixing the URL regex problem

--- django_poll/urls.py
+++ django_poll/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^polls/', include('polls.urls', namespace="polls")),
+    url(r'^/$', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 )
